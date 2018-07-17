22bd6244841ca7200826bea474778166754c311b
shinechlu <shinechlu@iii.org.tw>
2018-07-17 22:24:59 +0800

    first commit

---
+++ canvas-game/README.md
@@ -0,0 +1,21 @@
+# project
+前端修練精神時光屋
+第7週
+
+https://hackmd.io/N5yEjm2vSx6D41qAbJGDmw
+
+## install
+```
+cd canvas-game
+npm install
+```
+
+## develop
+```
+npm run serve
+```
+
+## deploy
+```
+npm run build
+```
---
+++ canvas-game/src/App.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  div#app
+    div.game
+</template>
+
+<script>
+import * as PIXI from 'pixi.js';
+
+export default {
+  name: 'app',
+  components: {},
+  data() {
+    return {      
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.init();
+    });
+  },
+  methods: {
+    init() {
+      // The application will create a renderer using WebGL, if possible,
+      // with a fallback to a canvas render. It will also setup the ticker
+      // and the root stage PIXI.Container
+      const app = new PIXI.Application();
+
+      // The application will create a canvas element for you that you
+      // can then insert into the DOM
+      document.querySelector(".game").appendChild(app.view);
+
+      // app.renderer.view.style.position = "absolute";
+      // app.renderer.view.style.display = "block";
+      console.log(app.renderer.view.style)
+      // app.renderer.autoResize = true;
+      // app.renderer.resize(window.innerWidth, window.innerHeight);
+      app.renderer.resize(1024, 800);
+    },
+
+  },
+  computed: {    
+    
+  },
+  filters: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+*{
+  margin:0;
+  padding:0;
+}
+#app{
+  display:flex;
+  justify-content: center;
+}
+</style>
